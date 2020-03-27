e6f56d1e84278f2afb28540471b7887728098f34
Gautam Dayal <gautam.dayal@yahoo.com>
2020-03-27 15:18:14 +0530

    completed Replicator class

--- existence/equilibrium.py
+++ existence/equilibrium.py
@@ -19,6 +19,20 @@ class Existor(object):
     def getEquilibrium(self):
         return(self.birth_rate/self.death_rate)
 
-# A more realistic version of Species as organisms can replicate
+# A more realistic version of the Species class as organisms can replicate
 class Replicator(Existor):
-    pass
+    def __init__(self, population, birth_rate, death_rate, replication_rate):
+        Existor.__init__(self, population, birth_rate, death_rate)
+        self.replication_rate = replication_rate
+
+    def update(self):
+        Existor.update(self)
+        for organism in range(self.population):
+            if random.randint(0, 100) < self.replication_rate:
+                self.population += 1
+
+    def getEquilibrium(self):
+        return(self.birth_rate/(self.death_rate - self.replication_rate))
+
+raindrop = Existor(0, 100, 10)
+pigeon = Replicator(3, 10, 5, 3)
