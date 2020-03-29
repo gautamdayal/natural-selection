c32fe8b42e9dcbf51be05284c1be3166171fa73d
Gautam Dayal <gautam.dayal@yahoo.com>
2020-03-29 22:54:55 +0530

    FINISHED THE EXISTENCE MODULEgit add .

--- existence/equilibrium.py
+++ existence/equilibrium.py
@@ -9,6 +9,7 @@ class Existor(object):
         self.birth_rate = birth_rate
         self.death_rate = death_rate
 
+    # Updates population based on birth and death rates
     def update(self):
         if random.randint(0, 100) < self.birth_rate:
             self.population += 1
@@ -16,20 +17,29 @@ class Existor(object):
             if random.randint(0, 100) < self.death_rate:
                 self.population -= 1
 
+    # Returns the predicted equilibrium value based on our equation
     def getEquilibrium(self):
         return(self.birth_rate/self.death_rate)
 
+    # Takes three arguments: number of cycles, whether to plot equilibrium, whether to plot mean
     def plotPopulation(self, cycles, with_equilibrium = False, with_mean = False):
-        X = [i for i in range(cycles)]
         Y = []
         for cycle in range(cycles):
             self.update()
             Y.append(self.population)
 
         plt.ylim(0, 5 * self.getEquilibrium())
-        plt.plot(X, Y)
+        plt.plot(Y, label='Population')
+
+        if with_mean:
+            mean = sum(Y)/len(Y)
+            plt.plot([mean for i in range(cycles)], label = 'Mean population')
+
         if with_equilibrium:
-            plt.plot([self.getEquilibrium() for i in range(cycles)])
+            plt.plot([self.getEquilibrium() for i in range(cycles)], label = 'Predicted equilibrium')
+        plt.xlabel('Cycles')
+        plt.ylabel('Number of organisms')
+        plt.legend()
         plt.show()
 
 # A more realistic child class of Species as organisms can replicate
@@ -38,6 +48,7 @@ class Replicator(Existor):
         Existor.__init__(self, population, birth_rate, death_rate)
         self.replication_rate = replication_rate
 
+    # Inherited from Existor but modified to include a replication rates
     def update(self):
         Existor.update(self)
         for organism in range(self.population):
@@ -50,5 +61,5 @@ class Replicator(Existor):
 raindrop = Existor(0, 100, 10)
 pigeon = Replicator(3, 10, 5, 3)
 
-raindrop.plotPopulation(500, True)
-# pigeon.plotPopulation(500, True)
+# raindrop.plotPopulation(500, True, True)
+pigeon.plotPopulation(500, True, True)
